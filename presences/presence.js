5f06a9594d65dc8d43b8dff096b8fa4ac2d0ff53
Lars_und_so <46791248+Larsundso@users.noreply.github.com>
2021-05-03 16:34:33 +0200

    Update presence.js

--- presences/presence.js
+++ presences/presence.js
@@ -1,19 +1,20 @@
-const { pool } = require('../Database.js');
 module.exports = {
 	name: 'presence',
 	async execute(client) {
-        const res = await pool.query(`SELECT * FROM stats`);
         client.setActivity({
-            largeImageKey: '512',
-            largeImageText: 'v1.4.-',
-            smallImageKey: 'lars_und_so',
-            smallImageText: 'Developed by Lars_und_so',
-                details: `${res.rows[0].guildcount} Servers | ${res.rows[0].usercount} Users | ${res.rows[0].rolecount} Roles`,
-            state: `${res.rows[0].allusers} Server Members | ${res.rows[0].channelcount} Channels`,
+            largeImageKey: 'Large Image Key', // Image Key
+            largeImageText: 'Large Image Text', // String
+            smallImageKey: 'Small Image Key', // Image Key
+            smallImageText: 'Small Image Text', // String
+            details: `Details`, // String
+            state: `State`, // String
             buttons: [
-                { label: "Invite Ayako", url: "https://discord.com/api/oauth2/authorize?client_id=650691698409734151&permissions=1576396919&scope=bot" },
+                { 
+		 label: "Button Label", // String 
+		 url: "Button URL" // URL
+		}, // You can have 2 Max Buttons
             ],
-            instance: false,
+            instance: false, // Boolean
         });
     }
 }
